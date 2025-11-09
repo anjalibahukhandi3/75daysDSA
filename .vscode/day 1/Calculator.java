710967ba95383c42a5f36296655f93bb37169f2e
anjali <anjali.bahukhandi2005@gmail.com>
2025-11-10 00:55:58 +0530

    debugged in day1

---
+++ .vscode/day 1/Calculator.java
@@ -0,0 +1,45 @@
+iimport java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        
+        System.out.println("Choose operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        
+        int operation = sc.nextInt();
+        
+        switch (operation) {
+            case 1:
+                System.out.println("Result: " + (a + b));
+                break;
+            case 2:
+                System.out.println("Result: " + (a - b));
+                break;
+            case 3:
+                System.out.println("Result: " + (a * b));
+                break;
+            case 4:
+                if (b != 0) {
+                    System.out.println("Result: " + ((double) a / b));
+                } else {
+                    System.out.println("Error: Division by zero!");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+
+        sc.close();
+    }
+}
