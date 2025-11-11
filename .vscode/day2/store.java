9f80d13b4dcce1314b0176ea9806b1b89faaff4f
anjali <anjali.bahukhandi2005@gmail.com>
2025-11-11 23:34:17 +0530

    day2

---
+++ .vscode/day2/store.java
@@ -0,0 +1,19 @@
+package .vscode.day2;
+
+public class store {
+    public static void main(String[] args) {
+        int n=5;
+        int roll =1234;
+        String name="Anjali";
+        float price=45.67f;
+        char grade='A';
+        System.out.println("Roll number:"+roll);
+        System.out.println("Name:"+name);
+        System.out.println("Price:"+price);
+        System.out.println("Grade:"+grade);
+        
+
+         
+    }
+    
+}
